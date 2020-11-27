911e5b6de24ddafc56f1ed35cab4a4520f6f66bd
Wenyu Zheng <zhengwy229@gmail.com>
2020-11-27 21:12:29 +0000

    outline homepage structure

--- Scam Baiter Website/app/controllers/home_controller.rb
+++ Scam Baiter Website/app/controllers/home_controller.rb
@@ -2,7 +2,28 @@ class HomeController < ApplicationController
 
   layout false
 
+  def generateGreeting
+    current_time = Time.now.to_i
+    midnight = Time.now.beginning_of_day.to_i
+    noon = Time.now.middle_of_day.to_i
+    five_pm = Time.now.change(:hour => 17 ).to_i
+    eight_pm = Time.now.change(:hour => 20 ).to_i
+
+    if midnight.upto(noon).include?(current_time)
+      @greeting = "Good Morning"
+    elsif noon.upto(five_pm).include?(current_time)
+      @greeting =  "Good Afternoon"
+    elsif five_pm.upto(eight_pm).include?(current_time)
+      @greeting = "Good Evening"
+    elsif eight_pm.upto(midnight + 1.day).include?(current_time)
+      @greeting = "Good Night"
+    end
+  end 
+
   def index
+    generateGreeting
+    @logged_in = false;
+    @username = "Wenyu";
   end
 
 end
