3f60276bfe102fdbe8fe7382649fdc90721547ef
Wenyu Zheng <zhengwy229@gmail.com>
2020-11-28 13:01:23 +0000

    implement contact page

--- Scam Baiter Website/config/routes.rb
+++ Scam Baiter Website/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
+  get 'contact/index'
+
   get 'profile/view'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
