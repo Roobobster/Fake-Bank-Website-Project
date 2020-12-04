cbe7792eee3f1798a02b7128684f0c4e18029f91
Andrei Cinca <andrei-marian.cinca@kcl.ac.uk>
2020-12-04 19:57:43 +0200

    added username changes for the current user

--- Scam Baiter Website/app/controllers/application_controller.rb
+++ Scam Baiter Website/app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  
   protect_from_forgery with: :exception
+
+helper_method :current_user
+helper_method :logged_in?
+
+  def current_user
+    if session[:user_id]
+        Profile.find_by(id: session[:user_id])
+    end
+end
+
+def logged_in?
+    !current_user.nil?
+end
 end
--- Scam Baiter Website/app/controllers/sessions_controller.rb
+++ Scam Baiter Website/app/controllers/sessions_controller.rb
@@ -5,12 +5,14 @@ class SessionsController < ApplicationController
 
   def create
     user=Profile.find_by(username: params[:session][:username].downcase)
-    status=Profile.find_by(admin: params[:session][:admin])
+   
+   
     if user && user.authenticate(params[:session][:password]) && user.admin==true
       log_in(user)
       redirect_to('http://localhost:3000')
+      
     else
-      redirect_to('http://localhost:3000/profile/view?first_name=Robert')
+      redirect_to('http://localhost:3000')
     end
   end
 
