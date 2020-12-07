1a5a3f86982dcbd57d34574ae1cb034aa3ad5896
Luke Waddington <k19013563@kcl.ac.uk>
2020-12-07 15:53:53 +0000

    Implement account view

---
+++ Scam Baiter Website/app/controllers/accounts_controller.rb
@@ -0,0 +1,27 @@
+class AccountsController < ApplicationController
+    #layout false
+    def view
+        profile_id = nil
+        if (helpers.logged_in?)
+            profile_id = helpers.current_user.profiles_id
+        else
+            #account1 = Account.create(profiles_id: 2, account_type: "Savings", amount: 0.0)
+            #account1.save
+            #profile_id = account1.profiles_id
+            profile_id = 2
+        end
+        p profile_id
+        @accountlist = Account.where('profiles_id = ?', profile_id) #This becomes the list of accounts the current profile has
+        p @accountlist.size
+    end
+
+    def new
+    
+    end
+
+    def index
+        view
+    end
+    
+  end
+  
